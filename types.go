4fae3371cbe12db1853b5099076fedb8362aa88b
Sungjin Han <meinside@gmail.com>
2018-07-16 12:03:34 +0900

    Edit typo

--- types.go
+++ types.go
@@ -556,13 +556,13 @@ type ShippingQuery struct {
 //
 // https://core.telegram.org/bots/api#precheckoutquery
 type PreCheckoutQuery struct {
-	ID                string     `json:"id"`
-	From              User       `json:"from"`
-	Currency          string     `json:"currency"`
-	TotalAmount       int        `json:"total_amount"`
-	InvoicePayload    string     `json:"invoice_payload"`
-	ShippingOptioniID *string    `json:"shipping_option_id,omitempty"`
-	OrderInfo         *OrderInfo `json:"order_info,omitempty"`
+	ID               string     `json:"id"`
+	From             User       `json:"from"`
+	Currency         string     `json:"currency"`
+	TotalAmount      int        `json:"total_amount"`
+	InvoicePayload   string     `json:"invoice_payload"`
+	ShippingOptionID *string    `json:"shipping_option_id,omitempty"`
+	OrderInfo        *OrderInfo `json:"order_info,omitempty"`
 }
 
 // ForceReply is a struct for force-reply
